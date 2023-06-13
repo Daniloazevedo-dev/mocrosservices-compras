e04051de67d1ab36528551c17555971ff0cae459
Danilo Azevedo <danilo.ann@gmail.com>
2023-06-13 20:52:01 -0300

    Ajuste import

--- validador/src/main/java/com/worker/validador/service/ValidadorService.java
+++ validador/src/main/java/com/worker/validador/service/ValidadorService.java
@@ -1,7 +1,7 @@
 package com.worker.validador.service;
 
+import com.worker.validador.model.Cartao;
 import com.worker.validador.model.Pedido;
-import com.workercompras.model.Cartao;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
